8cc785881ceb4370e6b5fbec94505dbc66ecb983
Polkaverse <pankajchaudhary172@gmail.com>
2024-05-23 08:58:52 +0530

    updated ccode with documentation

--- poker_activity/src/main.rs
+++ poker_activity/src/main.rs
@@ -19,19 +19,19 @@ fn main() {
     let keypair3 = Keypair::generate_with(&mut secure_rng);
     let keypair4 = Keypair::generate_with(&mut secure_rng);
 
-    // Draw cards for all players
+    // Draw cards for all players.
     let draw1 = draws(&keypair1, vrf_seed);
     let draw2 = draws(&keypair2, vrf_seed);
     let draw3 = draws(&keypair3, vrf_seed);
     let draw4 = draws(&keypair4, vrf_seed);
 
-    // Assuming we are only interested in the first draw
+    // Assuming we are only interested in the first draw.
     let (card1, signature1) = draw1[0] ;
     let (card2, signature2) = draw2[0] ;
     let (card3, signature3) = draw3[0] ;
     let (card4, signature4) = draw4[0] ;
 
-    // Creating a vector which consist each players draw cards
+    // Creating a vector which consist each players draw cards.
     let mut players = Vec::new() ;
     players.push((1, card1)) ;
     players.push((2, card2)) ;
@@ -48,7 +48,7 @@ fn main() {
         _ => println!("No winner"),
     }
 
-    // Optionally, revealing cards using receive function
+    // Optionally, revealing cards using receive function.
     let public_key1 = keypair1.public ;
     let public_key2 = keypair2.public ;
     let public_key3 = keypair3.public ;
@@ -65,7 +65,19 @@ fn main() {
     println!("Revealed card for player4: {}", reveal_card4.unwrap()) ;
 }
 
-
+/// Creates a transcript for a card draw operation.
+///
+/// This function generates a new transcript for a card draw operation using the provided seed and draw number.
+/// If the draw number exceeds the maximum allowed number of draws, the function returns None.
+///
+/// # Arguments
+///
+/// * seed - A 32-byte slice representing the seed used for the card draw.
+/// * draw_num - The number of the draw operation.
+///
+/// # Returns
+///
+/// An Option<Transcript> containing the generated transcript if the draw number is valid, or None otherwise.
 fn draw_transcript(seed: &[u8; 32], draw_num: u8) -> Option<Transcript> {
     if draw_num > NUM_DRAWS { return None; }
     let mut card_draw_transcript = Transcript::new(b"Card Draw Transcript");
@@ -74,12 +86,36 @@ fn draw_transcript(seed: &[u8; 32], draw_num: u8) -> Option<Transcript> {
     Some(card_draw_transcript)
 }
 
+/// Finds the card number based on VRF input/output.
+///
+/// This function calculates the card number based on the VRF input/output provided.
+/// The VRF input/output is converted into bytes using the specified label "card", and
+/// the card number is determined by performing a modulo operation on the resulting bytes.
+///
+/// # Arguments
+///
+/// * vrf_output - A reference to the VRF input/output (VRFInOut) from which the card number is derived.
 fn find_card(vrf_output: &VRFInOut) -> Option<u16> {
     let card_bytes: [u8; 8] = vrf_output.make_bytes(b"card");
     Some( (u64::from_le_bytes(card_bytes) % (NUM_CARDS as u64)) as u16 )
 }
 
-
+/// Tries to draw a card and generate a VRF signature.
+///
+/// This function attempts to draw a card by generating a VRF signature using the provided keypair,
+/// seed, and draw number. It constructs a transcript, signs it using the VRF keypair, and then
+/// generates a VRF signature containing the card information along with the proof and draw number.
+///
+/// # Arguments
+///
+/// * keypair - A reference to the keypair (Keypair) used to sign the transcript.
+/// * seed - A 32-byte slice representing the seed used for the card draw.
+/// * draw_num - The number of the draw operation.
+///
+/// # Returns
+///
+/// An Option containing a tuple (u16, [u8; 97]) representing the card number and VRF signature
+/// if the draw is successful, or None if any step of the process fails.
 fn try_draw(keypair: &Keypair, seed: &[u8; 32], draw_num: u8) -> Option<(u16, [u8; 97])> {
     let transcript = draw_transcript(seed, draw_num) ?;
     let (vrf_in_out, proof, _) = keypair.vrf_sign(transcript);
@@ -91,12 +127,53 @@ fn try_draw(keypair: &Keypair, seed: &[u8; 32], draw_num: u8) -> Option<(u16, [u
     Some((card, vrf_signature))
 }
 
-
+/// Draws multiple cards and generates VRF signatures.
+///
+/// This function draws multiple cards and generates VRF signatures for each draw operation
+/// using the provided keypair and seed. It iterates over the specified number of draws,
+/// attempting to draw a card and generate a VRF signature for each draw. Draws that fail
+/// to produce a valid signature are filtered out, and the successful draws are collected
+/// into a vector of tuples containing the card number and VRF signature.
+///
+/// # Arguments
+///
+/// * keypair - A reference to the keypair (Keypair) used to sign the transcripts.
+/// * seed - A 32-byte slice representing the seed used for the card draws.
+///
+/// # Returns
+///
+/// A vector of tuples (u16, [u8; 97]) representing the card numbers and VRF signatures
+/// for the successful draw operations.
 fn draws(keypair: &Keypair, seed: &[u8; 32],) -> Vec<(u16, [u8; 97])>{
     (0..NUM_DRAWS).filter_map(|draw_num| try_draw(keypair, seed, draw_num)).collect()
 }
 
+/// Verifies a card play using the provided public key, VRF signature, and seed.
+///
+/// This function reconstructs the transcript using the seed and the draw number
+/// extracted from the VRF signature, verifies the VRF proof, and then determines
+/// the card associated with the verified VRF input/output. It returns the card
+/// number if the verification is successful.
+///
+/// # Arguments
+///
+/// * public - A reference to the PublicKey used to verify the VRF proof.
+/// * vrf_signature - A slice of 97 bytes containing the VRF preoutput (32 bytes),
+///   the VRF proof (64 bytes), and the draw number (1 byte).
+/// * seed - A 32-byte slice representing the seed used to generate the transcript.
+///
+/// # Returns
+///
+/// An Option<u16> containing the card number if the verification is successful,
+/// or None if any step of the verification process fails.
+/// This function is to ensure the integrity and uniqueness of the data of draws.
+///
+/// Compute the actual card draw from VRF inputs and outputs,
+/// divide by the total card outcomes, and map to the drawn card index.
 
+/// This function will pick a card from the drawn cards.
+///
+/// This function Draws all our cards for the given seed.
 fn recieve(public: &PublicKey, vrf_signature: &[u8; 97], seed: &[u8; 32]) -> Option<u16>{
     let transcript = draw_transcript(seed, vrf_signature[96]) ?;
     let vrf_pre_out = VRFPreOut::from_bytes(&vrf_signature[..32]).ok() ?;
@@ -105,6 +182,20 @@ fn recieve(public: &PublicKey, vrf_signature: &[u8; 97], seed: &[u8; 32]) -> Opt
     find_card(&vrf_in_out)
 }
 
+/// Determines the winner among the players based on the highest card number.
+///
+/// This function takes a vector of player-card tuples and determines the winner based on
+/// the highest card number. If the vector is empty, indicating no players, it returns None.
+/// Otherwise, it returns a tuple containing the player ID and the card number of the winner.
+///
+/// # Arguments
+///
+/// * players - A vector of tuples (u8, u16) representing the player ID and their respective card numbers.
+///
+/// # Returns
+///
+/// An Option containing a tuple (u8, u16) representing the player ID and the card number of the winner,
+/// or None if there are no players.
 fn winner(players: Vec<(u8, u16)>) -> Option<(u8, u16)> {
     if players.is_empty(){
         None
